e66c623b22b7b100d73bea68e811cfd50e949b54
ReactNinja <jsninjacode@gmail.com>
2021-05-13 19:25:57 +0300

    s

--- 04_hoisnting/hoisting.js
+++ 04_hoisnting/hoisting.js
@@ -1,5 +1,23 @@
+// Вызов функции до ее объявления
+
+console.log(sum(1 , 2))
+
 function sum(a , b){
     return a + b
 }
 
-console.log(sum(1 , 2))
+
+// var  Пример 1
+
+console.log(i) // undefined
+
+var i = 42
+
+console.log(i) // 42
+
+//var Пример 2 
+
+var i
+console.log(i) // undefined 
+i = 42
+console.log(i) // 42
